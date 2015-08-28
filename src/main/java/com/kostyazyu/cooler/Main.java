84b813d95b5d2da06348022e7cf4626025d627b7
Konstantin Zyubin <kostyazyu@gmail.com>
2015-08-28 07:05:45 +0300

    Cooler. Initial commit

---
+++ src/main/java/com/kostyazyu/cooler/Main.java
@@ -0,0 +1,30 @@
+package com.kostyazyu.cooler;
+
+import java.util.function.Consumer;
+
+/**
+ * User: gkislin
+ * Date: 15.01.2015
+ *
+ * @link http://javawebinar.ru/topjava/
+ */
+public class Main {
+    public static void main(String[] args) {
+        execute(() -> {
+            System.out.println("Hello Topjava!");
+        });
+        consume(System.out::println, "Hello Topjava!");
+    }
+
+    private static void execute(Runnable runnable) {
+        System.out.println("Start runner");
+        runnable.run();
+        System.out.println("End runner");
+    }
+
+    private static <String> void consume(Consumer<String> consumer, String out) {
+        System.out.println("Start consume");
+        consumer.accept(out);
+        System.out.println("End consume");
+    }
+}
